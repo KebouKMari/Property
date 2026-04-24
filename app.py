385ead1f0b73c3980494b8f3e233dea95533241d
Marivone Kebou Kitio <83984841+KebouKMari@users.noreply.github.com>
2026-04-24 15:49:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 np.product = np.prod
-import matplotlib.pyplot as plt
 from rdkit.Chem import AllChem
 from rdkit import Chem
 from mordred import Calculator, descriptors
